748b4e4fcf08222b706ad45bbe941209652fb7c4
Dhrub <163623084+DhrubVij29@users.noreply.github.com>
2024-05-17 21:08:57 +0530

    Add files via upload

---
+++ palindrome.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class palindrome {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int n=sc.nextInt();
+        int temp=n;
+        int rev=0;
+        while(n>0){
+            int rem=n%10;
+            rev=rev*10+rem;
+            n=n/10;
+        }
+        if(temp==rev){
+            System.out.println("palindrome");
+        }
+        else{
+            System.out.println("not a palindrome");
+        }
+    
+}
+}
